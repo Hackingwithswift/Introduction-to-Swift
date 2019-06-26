8b0f6e57836edc129363056863d5d4e7bee51206
Veerababu Mulugu <veeru060786@gmail.com>
2019-06-26 10:12:21 -0600

    Added the functions

--- Functions&Optional.playground/Contents.swift
+++ Functions&Optional.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+
+//**************** Functions ******************************
+var name = "abcdefghijklmnopqrstuvwxyz"
+//print("My favorate Album")
+
+
+func favoriteAlbum() {
+    
+    
+    print("My favorate Album")
+    
+}
+
+func favoriteAlbum(name:String) {
+    
+    print("My favorite is \(name)")
+}
+func printAlbumRelease(name: String, year: Int) {
+    print("\(name) was released in \(year)")
+}
+
+printAlbumRelease(name: "Fearless", year: 2008)
+printAlbumRelease(name: "Speak Now", year: 2010)
+printAlbumRelease(name: "Red", year: 2012)
+
+
+//External and internal parameter names
+
+func countLetterInString(string: String){
+    print("the string \(string)has \(string.count) letters")
+    
+}
