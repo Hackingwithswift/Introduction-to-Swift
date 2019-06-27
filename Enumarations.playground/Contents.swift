7d972bdcc35c4902cf5ab1d12fe1d1bae853c91a
Veerababu Mulugu <veeru060786@gmail.com>
2019-06-27 15:50:34 -0600

    added the Enum samples

--- Enumarations.playground/Contents.swift
+++ Enumarations.playground/Contents.swift
@@ -6,7 +6,7 @@ import PlaygroundSupport
 class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = .red
 
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
@@ -19,3 +19,34 @@ class MyViewController : UIViewController {
 }
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
+
+
+// Enumurations
+//Enum
+
+func getHaterStatus(wheather: String) -> String? {
+    if wheather == "Sunny"{
+    return nil
+    } else{
+        return "Hate"
+    }
+
+}
+//
+enum WeatherType {
+    case sun, cloud, rain, wind, snow
+
+}
+func getHaterStatus(weather: WeatherType) -> String? {
+    if weather == WeatherType.sun {
+        return nil
+    } else{
+        return "Hate"
+        
+    }
+}
+
+getHaterStatus(weather: WeatherType.cloud)
+
+
+
